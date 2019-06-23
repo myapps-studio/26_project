4bf3a76448059849a5a361f49a5af055b47c4728
Andrzej Mieczkowski <praca.mieczkowski@gmail.com>
2019-06-23 15:36:45 +0200

    title problem

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import style from './App.css';
+import uuid from 'uuid';
+
+import Title from './components/Title';
 
 class App extends React.Component {
     constructor(props){
